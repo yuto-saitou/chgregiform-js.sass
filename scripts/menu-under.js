0e20b8e4722f9fe6d16b93c102c4e51f669dc81d
yutosaito <scrft1234@icloud.com>
2021-04-16 10:56:29 +0900

    登録情報変更画面

---
+++ scripts/menu-under.js
@@ -0,0 +1,26 @@
+console.log('接続テスト');
+// ロードが終わったら実行します？
+document.addEventListener('DOMContentLoaded',() =>{
+    const main = new Main();
+});
+
+//Mainインスタンスに動作を書く
+class Main{
+    //コンストラクタインスタンス生成時に一度だけ呼び出される
+    constructor(){
+        this.DOM = {};//DOMオブジェクト初期化値とキーを入れて使う
+        this._init();//initメソッド呼ぶ
+    }
+
+    _init(){
+        this.DOM.el = document.querySelector('.select-menu-btn');
+        //↓クリックした時のイベントを設定
+        this.DOM.el.addEventListener('click',this._cd.bind(this));
+
+    }
+
+    _cd(){//クリックするたびについたり消えたりさせる
+        document.querySelector('.select-menu').classList.toggle('open');
+    }
+}
+
